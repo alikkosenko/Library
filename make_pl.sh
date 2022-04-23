e6f7b79f20bfe43e6ad00b3a5c456b594bd8daf3
alikkosenko <alikkosenko31@gmail.com>
2022-04-23 19:28:10 +0300

    Some changes

--- make_pl.sh
+++ make_pl.sh
@@ -1,8 +1,8 @@
 #/usr/bin/bash
 
-export GIT_TRACE_PACKET=1
-export GIT_TRACE=1
-export GIT_CURL_VERBOSE=1
+#export GIT_TRACE_PACKET=1
+#export GIT_TRACE=1
+#export GIT_CURL_VERBOSE=1
 
 git add .
 git commit -m "Some changes"
