b69697361bb77296a1ee9acf8e5b0caed6075be3
root <root@winder.localdomain>
2022-11-07 03:59:55 +0200

    Some changes

--- make_pl.sh
+++ make_pl.sh
@@ -8,5 +8,5 @@ git add .
 git status
 git commit -m "Some changes"
 echo "token: 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"
-#git push origin main
-git push https://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@github.com/alikkosenko/library.git
+git push origin main
+#git push https://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@github.com/alikkosenko/library.git
